d1a324de7193863344f7babff0a0e5bb962251ce
룰루랄라 한기쌤 <yeohanki@naver.com>
2025-11-03 15:53:25 +0900

    build: Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          600: '#4f46e5',
+          700: '#4338ca',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
